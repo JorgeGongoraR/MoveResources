00e2175827cfc1f2663364c16d2b58d228e22ecf
c028694 <jorge.gongora@extcom.com>
2022-04-04 15:39:52 -0500

    Adding folder and main.tf

---
+++ mythirdrg/main.tf
@@ -0,0 +1,50 @@
+# Configuration
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.0.2"
+    }
+  }
+}
+
+# Variables
+variable "region" {
+  type        = string
+  description = "Region in Azure"
+  default     = "eastus"
+}
+
+variable "firstVariable" {
+  type        = string
+  description = "prefix for naming"
+  default     = "firstVariable"
+}
+
+variable "secondVariable" {
+  type        = string
+  description = "prefix for naming"
+  default     = "secondVariable"
+}
+
+# Providers
+provider "azurerm" {
+  features {}
+}
+
+# Data sources
+locals {
+  firstVariable = "${var.firstVariable}-demo"
+  secondVariable = "${var.secondVariable}-demo"
+}
+
+# Resources
+resource "azurerm_resource_group" "myfirstrg" {
+  name     = local.firstVariable
+  location = var.region
+}
+
+resource "azurerm_resource_group" "mythirdrg" {
+  name     = local.secondVariable
+  location = var.region
+}
